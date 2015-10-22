f736cb8d34f4ed5c963dd8149d90fa9d6c840f9f
Erlend Vie <erlend_t_vie@msn.com>
2015-10-22 11:44:50 +0200

    trenger ikke ha admin brukeren

--- src/webapp/Sql.php
+++ src/webapp/Sql.php
@@ -35,21 +35,17 @@ class Sql
 
     static function insertDummyUsers()
     {
-		$salt1 = UserController::generateSalt();
 		$salt2 = UserController::generateSalt();
 		$salt3 = UserController::generateSalt();
         $salt4 = UserController::generateSalt();
-        $hash1 = Hash::make(bin2hex(openssl_random_pseudo_bytes(2)), $salt1);
         $hash2 = Hash::make('bobdylan', $salt2);
         $hash3 = Hash::make('liverpool', $salt3);
         $hash4 = Hash::make('Testuser123', $salt4);
 
-        $q1 = "INSERT INTO users(user, pass, isadmin, fullname, address, postcode, salt, isdoctor) VALUES ('admin', '$hash1', 1, 'admin', 'homebase', '9090', '$salt1', 0)";
         $q2 = "INSERT INTO users(user, pass, isadmin, fullname, address, postcode, salt, isdoctor) VALUES ('bob', '$hash2', 0, 'Robert Green', 'Greenland Grove 9', '2010', '$salt2', 0)";
         $q3 = "INSERT INTO users(user, pass, isadmin, fullname, address, postcode, salt, cardnumber, isdoctor) VALUES ('bjarni', '$hash3', 0, 'Bjarni Torgmund', 'Hummerdale 12', '4120', '$salt3', 12345678912345, 0)";
         $q4 = "INSERT INTO users(user, pass, isadmin, fullname, address, postcode, salt, isdoctor) VALUES ('testuser', '$hash4', 1, 'admin', 'homebase', '9090', '$salt4', 0)";
-
-        self::$pdo->exec($q1);
+        
         self::$pdo->exec($q2);
         self::$pdo->exec($q3);
         self::$pdo->exec($q4);
